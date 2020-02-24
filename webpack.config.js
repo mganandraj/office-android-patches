2ebafa09ecfbc502a3abefb21cdf7136d0680467
Anandraj <anandrag@microsoft.com>
2020-02-24 12:56:35 -0800

    CHecking in the bundle

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'bundle'),
   },
   externals: [nodeExternals()],
 };
